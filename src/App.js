0a72d4c5393b40c527a036f64667d3f3357c0c10
xiongqiang <qxiong@optikom.cn>
2020-12-24 14:18:30 +0800

    chore: mobx and mobx-react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+// App.js
+import React from 'react'
+import { makeAutoObservable } from "mobx"
+import { observer } from "mobx-react"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class Timer {
+  secondsPassed = 0
+
+  constructor () {
+    makeAutoObservable(this)
+  }
+
+  increaseTimer () {
+    this.secondsPassed += 1
+  }
 }
 
-export default App;
+const myTimer = new Timer()
+
+function App () {
+  return (
+    <div>
+      <div onClick={() => myTimer.increaseTimer()}>click me</div>
+      <div>myTimer: {myTimer.secondsPassed}</div>
+    </div>
+  )
+}
+
+export default observer(App)
